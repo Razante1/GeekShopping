81f6f4ddc34f31d171cc599e0a1d83d3daca3594
Antonio Guilherme Macedo Camara <antonioguilhermemc@gmail.com>
2023-08-10 17:28:12 -0300

    Fisrt Commit GeekShopping

---
+++ GeekShopping.Web/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using GeekShopping.ProductAPI.Model.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GeekShopping.Web.Models
+{
+    [Table("product")]
+    public class Product : BaseEntity
+    {
+        [Column("name")]
+        [Required]
+        [StringLength(150)]
+        public int Name{ get; set; }
+
+        [Column("price")]
+        [Required]
+        [Range(1,10000)]
+        public decimal Price { get; set; }
+    }
+}
